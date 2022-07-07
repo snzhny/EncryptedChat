9d79b06bd935a75c20f22c77f904250ffbbcb8ed
hineko <cnxnd11@gmail.com>
2022-07-07 17:35:46 +0300

    chat(server-side)

--- chat.py
+++ chat.py
@@ -1,8 +1,40 @@
+#!/usr/bin/python3
+"""
+Server-side chat
+"""
 try:
-    import socket, sys, time, tor
+    import socket
+    import sys
+    import tor
+    import time
 except ModuleNotFoundError:
     from subprocess import call
+
     modules = ["tor"]
     call("pip install " + ' '.join(modules), shell=True)
 finally:
-    pass
+    server_socket = socket.socket()
+    port = 8080
+    server_host = socket.gethostname()
+    server_ip = socket.gethostbyname(server_host)
+    numberOfConnections = 2
+
+    server_socket.bind((server_host, port))
+
+    client_name = input("Enter your name: ")
+    server_socket.listen(numberOfConnections)
+
+    conn, address = server_socket.accept()
+    print(f"Received connection from: {address[0]}")
+
+    client = (conn.recv(1024)).decode()
+    print(client + ' has connected.')
+    conn.send(client_name.encode())
+
+    while True:
+        message = input("Me > ")
+        conn.send(message.encode())  # we need to create a custom encode func
+        message = conn.recv(1024)  # message size: 1Kb
+        message = message.decode()  # we need to create a custom decode func
+
+        print(f"{client} > {message}")
