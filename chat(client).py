0bd0d94b963f1a8c1ca698b2c7945247595c9f65
4eburaWka <108889278+4eburaWka@users.noreply.github.com>
2022-07-07 20:23:32 +0300

    Update chat(client).py

--- chat(client).py
+++ chat(client).py
@@ -28,18 +28,17 @@ finally:
     server_name = server_name.decode()
 
     def escape():
-        if keyboard.is_pressed('Esc'):
-            client_socket.close()
+        while True:
+            if keyboard.is_pressed('Esc'):
+                client_socket.close()
 
     print("You joined")
     print("To escape of this chat press: ESC")
 
     if "__name__" == "__main__":
+        Thread(target=escape).start()
         while True:
             message = (client_socket.recv(1024)).decode()
             print(server_name, ">", message)
             message = input("me > ")
             client_socket.send(message.encode())
-
-    th = Thread(target=escape)
-    th.start()
