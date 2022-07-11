42ae71249d37e0631a3c088eed289f15dc84824d
4eburaWka <108889278+4eburaWka@users.noreply.github.com>
2022-07-11 18:33:52 +0300

    Update and rename encrypt_and_decrypt.py to encrypting.py

--- encrypting.py
+++ encrypting.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 try:
     from cryptography.fernet import Fernet
     from random import randint
@@ -11,56 +9,35 @@ except ModuleNotFoundError:
     call("pip install " + ' '.join(modules), shell=True)
 finally:
     # base64 encryption
-    def encryptFile(path, key):
+    def encryptFile(path : str, key : int) -> bytes:
         with open(path, 'rb') as file:
             file_encode_b64 = base64.b64encode(file.read())
         keyraw = '{:032b}'.format(key)
         fernet = Fernet(base64.urlsafe_b64encode(bytes(keyraw, encoding='utf8')))
         encrypted = fernet.encrypt(file_encode_b64)
-        print(encrypted) # это мы отправляем челу
-        # return encrypted
+        # print(encrypted) # это мы отправляем челу
+        return encrypted
 
     # base64 decryption
     def decryptFile(encrypted, key):
-        keyraw = '{:032b}'.format(key)
-        fernet = Fernet(base64.urlsafe_b64encode(bytes(keyraw, encoding='utf8')))
+        keyraw = '{:032b}'.format(int(key))
+        fernet = Fernet(base64.urlsafe_b64encode(bytes(keyraw, encoding='utf-8')))
         decrypted = fernet.decrypt(encrypted)
 
-        with open("a.png", 'wb') as file:
+        with open("D:\\de.txt", 'wb') as file:
             file.write(base64.b64decode(decrypted))
 
-        print(decrypted)
+        return f'file on D:\\de.txt'
 
     # шифровка текста
-    def encryptText(text, key):
+    def encryptText(text : str, key : int) -> bytes :
         keyraw = '{:032b}'.format(key)
         fernet = Fernet(base64.urlsafe_b64encode(bytes(keyraw, encoding='utf8')))
-        encrypted = fernet.encrypt(text.encode())
+        encrypted = fernet.encrypt(text.encode('utf-8'))
         return encrypted
 
-    def decryptText(encrypted, key):
+    def decryptText(encrypted : bytes, key : int) -> str:
         keyraw = '{:032b}'.format(key)
         fernet = Fernet(base64.urlsafe_b64encode(bytes(keyraw, encoding='utf8')))
         encrypted = fernet.decrypt(encrypted).decode()
         return encrypted
-
-    # g = randint(1, 10)
-    # p = randint(1, 10)
-    #
-    # # A
-    # a = randint(1, 10)
-    # ga = (g ** a) % p
-    #
-    # # B
-    # b = randint(1, 10)
-    # gb = (g ** b) % p
-    #
-    # # KEYS
-    # key_a = gb ** a % p
-    # key_b = ga ** b % p
-
-    #
-    # file_encode_b64 = intoBase64()
-    # encrypted = encrypt(key_a, file_encode_b64)
-    # decrypted = decrypt(key_b, encrypted)
-    # fromBase64(decrypted)
