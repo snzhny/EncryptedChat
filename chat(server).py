12c717d6a29447dec1aa65e819528f5148226d03
4eburaWka <108889278+4eburaWka@users.noreply.github.com>
2022-07-12 14:39:18 +0300

    Update chat(server).py

--- chat(server).py
+++ chat(server).py
@@ -10,7 +10,9 @@ def quit1():
         print(5675454)
         flag = False
         for userik in users:
+            Thread(target=start_server, args=(userik,)).join()
             Thread(target=resend, args=(userik,)).join()
+        sock.close()
         quit()
         call("exit")
 
@@ -24,7 +26,9 @@ def resend(user):
                 data += user.recv(1024)
                 count += 1
         except ConnectionResetError:
-            print(f"chel vishel")
+            print(f"{names[user]} vishel")
+            users.remove(user)
+            del names[user]
             break
         str = names[user] + '`!~!`'.encode('utf-8') + data # стоп-слово, которое отделяет имя от данных
         for use in users:
@@ -35,15 +39,16 @@ def resend(user):
 
 def start_server():
     while flag:
-        user_socket, address = sock.accept()
-        print(f"User <{address[0]}> connected")
-        user_socket.send(f"{g} {p}".encode())
-        data_gb = user_socket.recv(1024)
+        if len(names) < numberOfConnections:
+            user_socket, address = sock.accept()
+            print(f"User <{address[0]}> connected")
+            user_socket.send(f"{g} {p}".encode())
+            data_gb = user_socket.recv(1024)
 
-        user_socket.send(data_gb)
-        names[user_socket] = user_socket.recv(256)  # имя пользователя
-        users.append(user_socket)  # ip пользователя
-        Thread(target=resend, args=(user_socket,)).start()
+            user_socket.send(data_gb)
+            names[user_socket] = user_socket.recv(256)  # имя пользователя
+            users.append(user_socket)  # ip пользователя
+            Thread(target=resend, args=(user_socket,)).start()
 
 
 if __name__ == '__main__':
@@ -54,7 +59,7 @@ if __name__ == '__main__':
     server_ip = socket.gethostbyname(server_host)
     print(f"IP of the server : {server_ip}")
     Thread(target=quit1).start()
-    numberOfConnections = 2
+    numberOfConnections = 3
     sock.bind((server_host, port))
     g = randint(1, 10)  # for keys
     p = randint(1, 10)  # for keys
