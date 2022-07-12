400de77bf338e81d7c6dd7dcbfd0f881782e958a
4eburaWka <108889278+4eburaWka@users.noreply.github.com>
2022-07-12 10:45:35 +0300

    Update chat(client).py

--- chat(client).py
+++ chat(client).py
@@ -1,5 +1,7 @@
+from http import client
 from random import randint, random
 from threading import Thread
+from tracemalloc import stop
 from encrypting import *
 import socket
 
@@ -8,24 +10,42 @@ def send_message():
     while True:
         message = input("me > ")
         if '!file' in message:
-            client_socket.send('file'.encode('utf-8')) # указывает на то, что будет отправлен файл
-            client_socket.send(encryptFile(message[6:], key))
+            encrypted_file = encryptFile(message[6:], key)
+            i = 0
+            for i in range(len(encrypted_file) // 1024):
+                client_socket.send(encrypted_file[i*1024:(i+1)*1024]) # отправляет файл чистично(по 1024 кБ)
+            client_socket.send(encrypted_file[-1024:] + 'file!StOp!'.encode('utf-8')) 
         else:
-            client_socket.send('text'.encode('utf-8')) # указывает на то, что будет отправлен text
-            client_socket.send(encryptText(message, key))  # encrypting.intoBase64(message[5:]).encode('utf-8'))
+            encrypted_text = encryptText(message, key)
+            print(encrypted_text)
+            i = 0
+            for i in range(len(encrypted_text) // 1024):                
+                client_socket.send(encrypted_text[i*1024:(i+1)*1024]) # отправляет файл чистично(по 1024 кБ)
+            client_socket.send(encrypted_text[-1024:] + 'text!StOp!'.encode('utf-8')) 
+
+            
+            
+            # client_socket.send('text'.encode('utf-8')) # указывает на то, что будет отправлен text
+            # client_socket.send(encryptText(message, key) + '!StOp!text'.encode('utf-8'))  # encrypting.intoBase64(message[5:]).encode('utf-8'))
 
 
 
 def receive_message():
     while True:
-        type = client_socket.recv(100).decode('utf-8') # тип файла
-        data = client_socket.recv(4096)
-        name = client_socket.recv(256).decode('utf-8')
+        data = ''
+        while '!StOp!' != data[-6:]: # пока не найдет стоп-слово будет принимать данные
+            data += client_socket.recv(1024).decode('utf-8')
+        stop_symb_name = data.find('`!~!`') # стоп для отделения имени
+        name = data[:stop_symb_name]
+        data = data[stop_symb_name:] # удаление имени из строки
+        data = data.replace('`!~!`', '') # удаление стоп-символа
+        type = data[-10:-6] # тип данных
+        data = data[:-10]
+        # print(data)
         if type == 'file':
-            print(f"{name} > {decryptFile(data, key)}")
+            print(f"{name} > {decryptFile(data.encode('utf-8'), key)}")
         else:
-            decryptedData = decryptText(data, key)
-            print(f"{name} > {decryptedData}")
+            print(f"{name} > {decryptText(data.encode('utf-8'), key)}")
 
 
 
