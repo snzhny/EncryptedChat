444891d557e02c915b53bcc31e2fbda9e4d60565
hineko <88938702+snzhny@users.noreply.github.com>
2022-07-20 14:01:24 +0300

    server

--- chat(server).py
+++ chat(server).py
@@ -1,13 +1,10 @@
 import socket
 from random import randint
-from subprocess import call
 from threading import Thread
 import keyboard
 
-
 def quit1():
     if not keyboard.wait('Esc'):
-        print(5675454)
         flag = False
         for userik in users:
             Thread(target=start_server, args=(userik,)).join()
@@ -15,53 +12,62 @@ def quit1():
         sock.close()
         quit()
 
-
 def resend(user):
     while True:
         try:
             data = ''.encode('utf-8')
             count = 0
-            while '!StOp!'.encode('utf-8') != data[-6:]: # пока не найдет стоп-слово будет принимать данные
+            while '!StOp!'.encode('utf-8') != data[-6:]:  # until it finds a stop word it will accept data
                 data += user.recv(1024)
                 count += 1
         except ConnectionResetError:
-            print(f"{names[user]} vishel")
+            print(f"{names[user].decode()} vishel")
             users.remove(user)
             del names[user]
             break
-        str = names[user] + '`!~!`'.encode('utf-8') + data # стоп-слово, которое отделяет имя от данных
+        str = names[user] + '!~!'.encode('utf-8') + data  # stopword that separates the name from the data
         for use in users:
             if use != user:
-                for i in range(count+1):
-                    use.send(str[i*1024:(i+1)*1024]) # отправляет файл чистично(по 1024 кБ)
-           # отправляет всем юзерам, кроме того кто это отправил
+                for i in range(count + 1):
+                    use.send(str[i * 1024:(i + 1) * 1024])  # partially sends a file(1024 kb)
+        # mailing to all users except sender
 
 def start_server():
+    # first user connection
+    user_socket, address = sock.accept()
+    names[user_socket] = user_socket.recv(256)  # user name
+    print(f"User <{names[user_socket].decode()}> connected")
+    user_socket.send(f"{g} {p}".encode())
+    user_ga = user_socket.recv(1024)
+    users.append(user_socket)  # user ip
+
     while flag:
         if len(names) < numberOfConnections:
             user_socket, address = sock.accept()
-            print(f"User <{address[0]}> connected")
+            names[user_socket] = user_socket.recv(256)
+            print(f"User <{names[user_socket].decode()}> connected")
             user_socket.send(f"{g} {p}".encode())
-            data_gb = user_socket.recv(1024)
+            user_gb = user_socket.recv(1024)
+            user_socket.send(user_ga)
 
-            user_socket.send(data_gb)
-            names[user_socket] = user_socket.recv(256)  # имя пользователя
-            users.append(user_socket)  # ip пользователя
+            if len(names) == 2:  # second user
+                users[0].send(user_gb)
+                Thread(target=resend, args=(users[0],)).start()
+
+            users.append(user_socket)
             Thread(target=resend, args=(user_socket,)).start()
 
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port = 8080
-    server_host = "192.168.100.33"
-    # server_host = socket.gethostname()
-    server_ip = socket.gethostbyname(server_host)
-    print(f"IP of the server : {server_ip}")
+    server_host = "192.168.0.82"
+    print(f"IP of the server : {server_host}")
     Thread(target=quit1).start()
     numberOfConnections = 3
     sock.bind((server_host, port))
-    g = randint(1, 10)  # for keys
-    p = randint(1, 10)  # for keys
+    g = randint(1000, 1000000)  # for keys
+    p = randint(1000, 1000000)
     sock.listen(numberOfConnections)
     users = []
     names = {}  # usernames of clients
