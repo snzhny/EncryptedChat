c777f0c64a3e79a923cedfe1b0c430bf3bc16392
4eburaWka <108889278+4eburaWka@users.noreply.github.com>
2022-07-26 23:23:36 +0300

    Update chat(client).py

--- chat(client).py
+++ chat(client).py
@@ -2,7 +2,7 @@ from threading import Thread
 from encrypting import *
 import socket
 from os import system
-import os
+
 
 def send_message():
     global history
@@ -22,12 +22,12 @@ def send_message():
             encrypted_file = encryptFile(message[6:], key)
             for i in range(len(encrypted_file) // 1024):
                 client_socket.send(encrypted_file[i * 1024:(i + 1) * 1024])  # partially sends a file(1024 kb)
-            client_socket.send(encrypted_file[-1024:] + fileType.encode('utf-8') + 'file!StOp!'.encode('utf-8'))
+            client_socket.send(encrypted_file[ -(len(encrypted_file) % 1024):] + ' _ '.encode() + fileType.encode('utf-8') + 'file!StOp!'.encode('utf-8'))
         else:
             encrypted_text = encryptText(message, key)
             for i in range(len(encrypted_text) // 1024):
                 client_socket.send(encrypted_text[i * 1024:(i + 1) * 1024])  # partially sends a text(1024 kb)
-            client_socket.send(encrypted_text[-1024:] + 'text!StOp!'.encode('utf-8'))
+            client_socket.send(encrypted_text[ -(len(encrypted_text) % 1024):] + 'text!StOp!'.encode('utf-8'))
         system('cls')
         print(history)
         print('me > ', end='')
@@ -40,13 +40,13 @@ def receive_message():
         fileType = ''
         while '!StOp!' != data[-6:]:  # until it finds a stop word it will accept data
             data += client_socket.recv(1024).decode('utf-8')
-        fileType=data[data.find('==')+2:data.find('file!StOp!')]#получение название и тип файла
+        fileType=data[data.find(' _ ')+3:-10]#получение название и тип файла
         stop_symb_name = data.find('!~!')  # stop to separate name
         name = data[:stop_symb_name]
         data = data[stop_symb_name:]  # removing a name from a string
         data = data.replace('!~!', '')  # remove stop-symbol
         dataType = data[-10:-6]
-        data = data[:-10]
+        data = data[:data.find(' _ ')]
 
         if dataType == 'file':
             history += f"{name} > {decryptFile(data.encode('utf-8'), key, filePath, fileType)}" + '\n'
@@ -60,7 +60,7 @@ def receive_message():
 history = ''
 if __name__ == '__main__':
     name = input('Enter your name: ')
-    server_host = input('Enter ip of server: ').strip()
+    server_host = '192.168.1.6'#input('Enter ip of server: ').strip()
     server_port = 8080
 
     client_socket = socket.socket()
@@ -76,4 +76,3 @@ if __name__ == '__main__':
 
     Thread(target=send_message).start()
     Thread(target=receive_message).start()
-
